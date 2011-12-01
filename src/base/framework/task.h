81dcf5dcb6dabe85d7f1d9d688b104431f2e8662
tangjie <tangjie.cqu163.com>
2011-12-01 21:08:33 +0800

    add runnable method and function,they can accept nine input params,but no output params

--- src/base/framework/task.h
+++ src/base/framework/task.h
@@ -39,6 +39,7 @@ namespace base {
 		}
 
 		virtual void Run() {
+			// TODO(tangjie):the object may be destructed. so we need to check its validity.
 			if (obj_) {
 				DispatchToMethod(obj_,method_,params_);
 			}
@@ -70,6 +71,47 @@ namespace base {
 		return std::shared_ptr<CancelableTask>(new RunnableMethod<T, Method, std::tr1::tuple<A, B>>(obj, method, std::tr1::make_tuple(a, b)));
 	}
 
+	template<class T, class Method, class A, class B, class C>
+	inline std::shared_ptr<CancelableTask> MakeRunnableMethod(T *obj, Method method, const A &a, const B &b, const C &c) {
+		return std::shared_ptr<CancelableTask>(new RunnableMethod<T, Method, std::tr1::tuple<A, B, C>>(obj, method, std::tr1::make_tuple(a, b, c)));
+	}
+
+	template<class T, class Method, class A, class B, class C, class D>
+	inline std::shared_ptr<CancelableTask> MakeRunnableMethod(T *obj, Method method, const A &a, const B &b, const C &c, const D &d) {
+		return std::shared_ptr<CancelableTask>(new RunnableMethod<T, Method, std::tr1::tuple<A, B, C, D>>(obj, method, std::tr1::make_tuple(a, b, c, d)));
+	}
+
+	template<class T, class Method, class A, class B, class C, class D, class E>
+	inline std::shared_ptr<CancelableTask> MakeRunnableMethod(T *obj, Method method, const A &a, const B &b, const C &c, const D &d, const E &e) {
+		return std::shared_ptr<CancelableTask>(new RunnableMethod<T, Method, std::tr1::tuple<A, B, C, D, E>>(obj, method, std::tr1::make_tuple(a, b, c, d, e)));
+	}
+
+	template<class T, class Method, class A, class B, class C, class D, class E, class F>
+	inline std::shared_ptr<CancelableTask> MakeRunnableMethod(T *obj, Method method, const A &a, const B &b, const C &c, const D &d, const E &e, const F &f) {
+		return std::shared_ptr<CancelableTask>(new RunnableMethod<T, Method, std::tr1::tuple<A, B, C, D, E, F>>(obj, method, std::tr1::make_tuple(a, b, c, d, e, f)));
+	}
+
+	template<class T, class Method, class A, class B, class C, class D, class E, class F, class G>
+	inline std::shared_ptr<CancelableTask> MakeRunnableMethod(T *obj, Method method, const A &a, const B &b, const C &c, const D &d, const E &e, const F &f,
+		const G &g) {
+		return std::shared_ptr<CancelableTask>(new RunnableMethod<T, Method, std::tr1::tuple<A, B, C, D, E, F, G>>(obj, method, std::tr1::make_tuple(a, b, c, d, e, f,
+			g)));
+	}
+
+	template<class T, class Method, class A, class B, class C, class D, class E, class F, class G, class H>
+	inline std::shared_ptr<CancelableTask> MakeRunnableMethod(T *obj, Method method, const A &a, const B &b, const C &c, const D &d, const E &e, const F &f,
+		const G &g, const H &h) {
+			return std::shared_ptr<CancelableTask>(new RunnableMethod<T, Method, std::tr1::tuple<A, B, C, D, E, F, G, H>>(obj, method, std::tr1::make_tuple(a, b, c, d, e, f,
+				g,h)));
+	}
+
+	template<class T, class Method, class A, class B, class C, class D, class E, class F, class G, class H, class I>
+	inline std::shared_ptr<CancelableTask> MakeRunnableMethod(T *obj, Method method, const A &a, const B &b, const C &c, const D &d, const E &e, const F &f,
+		const G &g, const H &h, const I &i) {
+			return std::shared_ptr<CancelableTask>(new RunnableMethod<T, Method, std::tr1::tuple<A, B, C, D, E, F, G, H, I>>(obj, method, std::tr1::make_tuple(a, b, c, d, e, f,
+				g,h, i)));
+	}
+
 	template<class Func, class Params>
 	class RunnableFunction : public CancelableTask {
 	public:
@@ -98,6 +140,57 @@ namespace base {
 	template<class Func>
 	inline std::shared_ptr<CancelableTask> MakeRunnableFunction(Func func) {
 		return std::shared_ptr<CancelableTask>(new RunnableFunction<func, std::tr1::tuple<std::tr1::_Nil>>(func, std::tr1::make_tuple(std::tr1::_Nil_obj)));
+	}
+
+	template<class Func, class A>
+	inline std::shared_ptr<CancelableTask> MakeRunnableFunction(Func func, const A &a) {
+		return std::shared_ptr<CancelableTask>(new RunnableFunction<func, std::tr1::tuple<A>>(func, std::tr1::make_tuple(a)));
+	}
+
+	template<class Func, class A, class B>
+	inline std::shared_ptr<CancelableTask> MakeRunnableFunction(Func func, const A &a, const B &b) {
+		return std::shared_ptr<CancelableTask>(new RunnableFunction<func, std::tr1::tuple<A, B>>(func, std::tr1::make_tuple(a, b)));
+	}
+
+	template<class Func, class A, class B, class C>
+	inline std::shared_ptr<CancelableTask> MakeRunnableFunction(Func func, const A &a, const B &b, const C &c) {
+		return std::shared_ptr<CancelableTask>(new RunnableFunction<func, std::tr1::tuple<A, B, C>>(func, std::tr1::make_tuple(a, b, c)));
+	}
+
+	template<class Func, class A, class B, class C, class D>
+	inline std::shared_ptr<CancelableTask> MakeRunnableFunction(Func func, const A &a, const B &b, const C &c, const D &d) {
+		return std::shared_ptr<CancelableTask>(new RunnableFunction<func, std::tr1::tuple<A, B, C, D>>(func, std::tr1::make_tuple(a, b, c, d)));
+	}
+
+	template<class Func, class A, class B, class C, class D, class E>
+	inline std::shared_ptr<CancelableTask> MakeRunnableFunction(Func func, const A &a, const B &b, const C &c, const D &d, const E &e) {
+		return std::shared_ptr<CancelableTask>(new RunnableFunction<func, std::tr1::tuple<A, B, C, D, E>>(func, std::tr1::make_tuple(a, b, c, d, e)));
+	}
+
+	template<class Func, class A, class B, class C, class D, class E, class F>
+	inline std::shared_ptr<CancelableTask> MakeRunnableFunction(Func func, const A &a, const B &b, const C &c, const D &d, const E &e, const F &f) {
+		return std::shared_ptr<CancelableTask>(new RunnableFunction<func, std::tr1::tuple<A, B, C, D, E, F>>(func, std::tr1::make_tuple(a, b, c, d, e, f)));
+	}
+
+	template<class Func, class A, class B, class C, class D, class E, class F, class G>
+	inline std::shared_ptr<CancelableTask> MakeRunnableFunction(Func func, const A &a, const B &b, const C &c, const D &d, const E &e, const F &f,
+		const G &g) {
+		return std::shared_ptr<CancelableTask>(new RunnableFunction<func, std::tr1::tuple<A, B, C, D, E, F, G>>(func, std::tr1::make_tuple(a, b, c, d, e, f, 
+			g)));
+	}
+
+	template<class Func, class A, class B, class C, class D, class E, class F, class G, class H>
+	inline std::shared_ptr<CancelableTask> MakeRunnableFunction(Func func, const A &a, const B &b, const C &c, const D &d, const E &e, const F &f,
+		const G &g, const H &h) {
+			return std::shared_ptr<CancelableTask>(new RunnableFunction<func, std::tr1::tuple<A, B, C, D, E, F, G, H>>(func, std::tr1::make_tuple(a, b, c, d, e, f, 
+				g, h)));
+	}
+
+	template<class Func, class A, class B, class C, class D, class E, class F, class G, class H, class I>
+	inline std::shared_ptr<CancelableTask> MakeRunnableFunction(Func func, const A &a, const B &b, const C &c, const D &d, const E &e, const F &f,
+		const G &g, const H &h, const I &i) {
+			return std::shared_ptr<CancelableTask>(new RunnableFunction<func, std::tr1::tuple<A, B, C, D, E, F, G, H, I>>(func, std::tr1::make_tuple(a, b, c, d, e, f, 
+				g, h, i)));
 	}
 }
 #endif// BASE_FRAMEWORK_TASK_H__
--- src/base/util/invoke_helper.h
+++ src/base/util/invoke_helper.h
@@ -30,9 +30,89 @@ namespace base {
 		(obj->*method)(get<0>(arg), get<1>(arg));
 	}
 
+	template<class T, class Method, class A, class B, class C>
+	inline void DispatchToMethod(T *obj, Method method, const tuple<A, B, C> &arg) {
+		(obj->*method)(get<0>(arg), get<1>(arg), get<2>(arg));
+	}
+
+	template<class T, class Method, class A, class B, class C, class D>
+	inline void DispatchToMethod(T *obj, Method method, const tuple<A, B, C, D> &arg) {
+		(obj->*method)(get<0>(arg), get<1>(arg), get<2>(arg), get<3>(arg));
+	}
+
+	template<class T, class Method, class A, class B, class C, class D, class E>
+	inline void DispatchToMethod(T *obj, Method method, const tuple<A, B, C, D, E> &arg) {
+		(obj->*method)(get<0>(arg), get<1>(arg), get<2>(arg), get<3>(arg), get<4>(arg));
+	}
+
+	template<class T, class Method, class A, class B, class C, class D, class E, class F>
+	inline void DispatchToMethod(T *obj, Method method, const tuple<A, B, C, D, E, F> &arg) {
+		(obj->*method)(get<0>(arg), get<1>(arg), get<2>(arg), get<3>(arg), get<4>(arg), get<5>(arg));
+	}
+
+	template<class T, class Method, class A, class B, class C, class D, class E, class F, class G>
+	inline void DispatchToMethod(T *obj, Method method, const tuple<A, B, C, D, E, F, G> &arg) {
+		(obj->*method)(get<0>(arg), get<1>(arg), get<2>(arg), get<3>(arg), get<4>(arg), get<5>(arg), get<6>(arg));
+	}
+
+	template<class T, class Method, class A, class B, class C, class D, class E, class F, class G, class H>
+	inline void DispatchToMethod(T *obj, Method method, const tuple<A, B, C, D, E, F, G, H> &arg) {
+		(obj->*method)(get<0>(arg), get<1>(arg), get<2>(arg), get<3>(arg), get<4>(arg), get<5>(arg), get<6>(arg), get<7>(arg));
+	}
+
+	template<class T, class Method, class A, class B, class C, class D, class E, class F, class G, class H, class I>
+	inline void DispatchToMethod(T *obj, Method method, const tuple<A, B, C, D, E, F, G, H, I> &arg) {
+		(obj->*method)(get<0>(arg), get<1>(arg), get<2>(arg), get<3>(arg), get<4>(arg), get<5>(arg), get<6>(arg), get<7>(arg), get<8>(arg));
+	}
+
 	template<class Func>
 	inline void DispatchToFunction(Func func, const tuple<std::tr1::_Nil> &arg) {
 		(*func)();
+	}
+
+	template<class Func,  class A>
+	inline void DispatchToFunction(Func func, const tuple<A> &arg) {
+		(*func)(get<0>(arg));
+	}
+
+	template<class Func,  class A, class B>
+	inline void DispatchToFunction(Func func, const tuple<A, B> &arg) {
+		(*func)(get<0>(arg), get<1>(arg));
+	}
+
+	template<class Func,  class A, class B, class C>
+	inline void DispatchToFunction(Func func, const tuple<A, B, C> &arg) {
+		(*func)(get<0>(arg), get<1>(arg), get<2>(arg));
+	}
+
+	template<class Func,  class A, class B, class C, class D>
+	inline void DispatchToFunction(Func func, const tuple<A, B, C, D> &arg) {
+		(*func)(get<0>(arg), get<1>(arg), get<2>(arg), get<3>(arg));
+	}
+
+	template<class Func,  class A, class B, class C, class D, class E>
+	inline void DispatchToFunction(Func func, const tuple<A, B, C, D, E> &arg) {
+		(*func)(get<0>(arg), get<1>(arg), get<2>(arg), get<3>(arg), get<4>(arg));
+	}
+
+	template<class Func,  class A, class B, class C, class D, class E, class F>
+	inline void DispatchToFunction(Func func, const tuple<A, B, C, D, E, F> &arg) {
+		(*func)(get<0>(arg), get<1>(arg), get<2>(arg), get<3>(arg), get<4>(arg), get<5>(arg));
+	}
+
+	template<class Func,  class A, class B, class C, class D, class E, class F, class G>
+	inline void DispatchToFunction(Func func, const tuple<A, B, C, D, E, F, G> &arg) {
+		(*func)(get<0>(arg), get<1>(arg), get<2>(arg), get<3>(arg), get<4>(arg), get<5>(arg), get<6>(arg));
+	}
+
+	template<class Func,  class A, class B, class C, class D, class E, class F, class G, class H>
+	inline void DispatchToFunction(Func func, const tuple<A, B, C, D, E, F, G, H> &arg) {
+		(*func)(get<0>(arg), get<1>(arg), get<2>(arg), get<3>(arg), get<4>(arg), get<5>(arg), get<6>(arg), get<7>(arg));
+	}
+
+	template<class Func,  class A, class B, class C, class D, class E, class F, class G, class H, class I>
+	inline void DispatchToFunction(Func func, const tuple<A, B, C, D, E, F, G, H, I> &arg) {
+		(*func)(get<0>(arg), get<1>(arg), get<2>(arg), get<3>(arg), get<4>(arg), get<5>(arg), get<6>(arg), get<7>(arg), get<8>(arg));
 	}
 }
 
