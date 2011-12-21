c2d0cabff48239205e3c2d13009b1660af0e0cf0
tangjie <tangjie.cqu@163.com>
2011-12-21 18:37:48 +0800

    add stop watch test

--- src/base/util/stop_watch_unittest.cpp
+++ src/base/util/stop_watch_unittest.cpp
@@ -1,19 +1,45 @@
+#include <sstream>
 #include "base/util/stop_watch.h"
-
 #include "gtest/gtest.h"
 
-TEST(StopWatch, Fuzzy) {
-	base::ScopedStopWatch wScoped(base::StringPiece("foobar"));
-	base::StopWatch w(base::StringPiece("Fuzzy"));
-	w.Start();
-	Sleep(100);
-	w.Report();
-	Sleep(100);
-	w.Stop();
-	w.Report();
-	Sleep(100);
-	w.Report();
-	w.Start();
-	Sleep(100);
-	w.Report();
+
+class ExpectReporter : public base::StopWatchReporter {
+public:
+	ExpectReporter(base::StringPiece watch_name) : watch_name_(watch_name) {
+	}
+
+	virtual void Report(const base::StringPiece& watch_name, base::TimeSpan elapsed) {
+		EXPECT_EQ(watch_name, watch_name_);
+		EXPECT_GT(elapsed.ToMilliseconds(), 0);
+	}
+private:
+	base::StringPiece watch_name_;
+};
+
+TEST(StopWatch, Basic) {
+	base::StopWatch watch(base::StringPiece("a"));
+	EXPECT_EQ(watch.name(), "a");
+	EXPECT_FALSE(watch.IsRunning());
+	watch.Start();
+	EXPECT_TRUE(watch.IsRunning());
+	watch.Stop();
+	EXPECT_FALSE(watch.IsRunning());
+	watch.Report();
+	watch.StopAndReset();
+	EXPECT_FALSE(watch.IsRunning());
+	watch.Restart();
+	EXPECT_TRUE(watch.IsRunning());
+}
+
+TEST(StopWatch, ScopedStopWatch) {
+	for (int i = 0; i < 10; ++i) {
+		std::stringstream ss;
+		ss << i;
+		std::string name = std::string("watch") + ss.str();
+		base::StringPiece watch_name(name);
+		ExpectReporter reporter(watch_name);
+		base::ScopedStopWatch watch(base::StringPiece(name), implicit_cast<base::StopWatchReporter*>(&reporter));
+		EXPECT_TRUE(watch.IsRunning());
+		Sleep(100);
+	}
 }
